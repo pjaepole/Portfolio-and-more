6ab7e3e12a5ce0c0f6c7c48103956376542d2ca8
Jaehun ParkLaptop <pjaehun4122@gmail.com>
2021-12-29 20:06:00 -0800

    checkbox error fixed

--- src/components/TodoRedux.js
+++ src/components/TodoRedux.js
@@ -1,3 +1,4 @@
+import {useState} from 'react'
 import {todoCompletedToggle,todoDelete} from '../actions'
 import {connect } from 'react-redux'
 import { InputGroup,
@@ -5,10 +6,12 @@ import { InputGroup,
         Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 function TodoRedux(props){
-    
+    const [check,setCheck]=useState(false)
     const {todo}=props
+    console.log(todo)
     let completeToggler=(e)=>{
         props.dispatch(todoCompletedToggle(todo.id))
+        setCheck(!check)
     }
     const deleteHandler=(e)=>{
         props.dispatch(todoDelete(todo.id))
@@ -18,14 +21,14 @@ function TodoRedux(props){
             
         {todo.completed?
             <InputGroup className="mb-3 todo">
-                <InputGroup.Checkbox aria-label="Checkbox for following text input" defaultChecked/>
+                <InputGroup.Checkbox aria-label="Checkbox for following text input" onChange={completeToggler} checked={check}/>
                 <FormControl className='todoC' onClick={completeToggler} defaultValue={todo.text} aria-label="Text input with checkbox" />
                 <Button onClick={completeToggler}>Done</Button>
                 <Button variant="danger" onClick={deleteHandler}>Delete</Button>
             </InputGroup>
             :
             <InputGroup className="mb-3">
-                <InputGroup.Checkbox aria-label="Checkbox for following text input" />
+                <InputGroup.Checkbox aria-label="Checkbox for following text input" onChange={completeToggler} checked={check}/>
                 <FormControl onClick={completeToggler} defaultValue={todo.text}  aria-label="Text input with checkbox" />
                 <Button onClick={completeToggler}>Done</Button>
                 <Button variant="danger" onClick={deleteHandler}>Delete</Button>
